afcf33000f06ed5f2232db2638822275c03b0686
ChristinaMac <Mac.christina852@gmail.com>
2016-11-03 15:55:55 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+/*var m = 16;
+var s = 17;
+
+var m = prompt ("5+4=");
+if (n==9) {
+comfirm ("yay you got it right");
+}
+else {
+confirm("you're bAD at math!!!!!!!!11!1111!")}*/
+
+function math() {
+var answer = prompt("whats the answer ho");
+if (answer == 2)
+{confirm ("cool")}
+}
